2c361d801ee72a076184146fc496a1d97e3cbd65
vaawebdev <vaawebdev@gmail.com>
2022-12-14 11:33:31 +0400

    IC

---
+++ tests/bt.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+
+describe("bt.ts", () => {
+  it("should sum the nodes in the tree", () => {
+    // @TODO
+    expect(true).toBeFalsy();
+  });
+});
